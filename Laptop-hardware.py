a49ffe469105a61d9d5dbb9454913121fe3a3415
titimariamumu <111260114+titimariamumu@users.noreply.github.com>
2023-12-20 16:00:19 -1000

    Update Laptop-hardware.py

--- Laptop-hardware.py
+++ Laptop-hardware.py
@@ -2,7 +2,33 @@ import random
 
 # Create a word bank
 
-word_bank = [ "apple" , "banana", "orange"]
+word_bank = [
+    # General nouns
+   "keyboard", "display", "battery", "lithium-ion battery", "lithium-ion polymer battery", "memory", "sodimm", 
+   "solid state drive (SSD)", "hard drive", "SATA", "m.2 interface", "chassis", "touchpad", "trackpad", "webcam",
+   "microphone", "speakers", "headphone jack", "USB ports", "Thunderbolt ports", "HDMI port", "Ethernet port", 
+   "SD card reader", "optical drive", "stylus",
+
+   # Display nouns
+   "resolution", "aspect ratio", "refresh rate", "touch screen", "brightness", "contrast", "color gamut", 
+   "viewing angles",
+
+   # Battery nouns
+   "capacity", "lifespan", "charging time", "quick charging", "battery health",
+
+   # Memory nouns
+   "RAM", "DDR4", "DDR5", "capacity", "speed", "channels",
+
+   # Storage nouns
+   "hard drive capacity", "SSD capacity", "NVMe", "SATA", "read speed", "write speed",
+
+   # Operating system nouns
+   "Windows", "macOS", "ChromeOS", "Linux", "updates", "security patches",
+
+   # Software nouns
+   "pre-installed software", "bloatware", "drivers", "firmware", "updates"
+   
+   ]
 
 # Randomly choose a word from word_bank
 
