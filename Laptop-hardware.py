7ef3766c1ec810d6819b72244fccfec3fe0b2492
titimariamumu <111260114+titimariamumu@users.noreply.github.com>
2023-12-20 15:44:58 -1000

    Create Laptop-hardware.py

---
+++ Laptop-hardware.py
@@ -0,0 +1,11 @@
+import random
+
+# Create a word bank
+
+word_bank = [ "apple" , "banana", "orange"]
+
+# Randomly choose a word from word_bank
+
+random_word = random.choice(word_bank)
+
+print (random_word)
